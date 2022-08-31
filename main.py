5096b06181d126f8b7fc81558fd59df4da33f4db
matszr <matszr660@gmail.com>
2022-08-31 21:34:33 +0200

    modify display_board

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ def display_board(board):
     
     print("+-------+-------+-------+")
     print("|       |       |       |")
-    print("|  ", board[0], "  |  " , board[1], "  |  " , board[2] , "  |" )
+    print("|  ", board[0, 0], "  |  " , board[0, 1], "  |  " , board[0, 2] , "  |" )
     print("|       |       |       |")
     print("+-------+-------+-------+")
     print("|       |       |       |")
-    print("|  ", board[3], "  |  " , board[4], "  |  " , board[5] , "  |" )
+    print("|  ", board[1, 0], "  |  " , board[1, 1], "  |  " , board[1, 2] , "  |" )
     print("|       |       |       |")
     print("+-------+-------+-------+")
     print("|       |       |       |")
-    print("|  ", board[6], "  |  " , board[7], "  |  " , board[8] , "  |" )
+    print("|  ", board[2, 0], "  |  " , board[2, 1], "  |  " , board[2, 2] , "  |" )
     print("|       |       |       |")
     print("+-------+-------+-------+")
     
